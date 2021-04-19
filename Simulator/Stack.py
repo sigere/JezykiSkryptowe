d65188428f8345b4ea99235f98b87b530f661f36
siger <a.marczak1324@gmail.com>
2021-04-19 12:08:08 +0200

    Merge with KK's part, bugfixes

--- Simulator/Stack.py
+++ Simulator/Stack.py
@@ -1,1 +1,74 @@
 class Stack:
+
+    # Initialize stack
+    def __init__(self, size):
+        self.s = [None] * size  # list to store stack elements
+        self.capacity = size  # maximum capacity of the stack
+        self.rear = -1  # rear points to the last element in the stack
+        self.count = 0  # current size of the stack
+        self.last_pop = None
+
+    # Function to remove the last element
+    def pop(self):
+        # check for stack underflow
+        if self.is_empty():
+            return False
+        value = self.s[self.rear]
+        self.rear = (self.rear - 1) % self.capacity
+        self.count = self.count - 1
+        self.last_pop = value
+        return value
+
+    # Function to add an element to the stack
+    def append(self, value):
+        # check for stack overflow
+        if self.is_full():
+            return False
+
+        self.rear = (self.rear + 1) % self.capacity
+        self.s[self.rear] = value
+        self.count = self.count + 1
+        return True
+
+    # Function to return last element of the stack
+    def peek(self):
+        if self.is_empty():
+            return False
+        return self.s[self.rear]
+
+    # Function to return the size of the stack
+    def size(self):
+        return self.count
+
+    # Function to check if the stack is empty or not
+    def is_empty(self):
+        return self.size() == 0
+
+    # Function to check if the stack is full or not
+    def is_full(self):
+        return self.size() == self.capacity
+
+    def sort(self):
+        new = []
+        i = 0
+        for s in range(self.count):
+            new.append(self.s[i])
+            i += 1
+        new.sort()
+        for s in range(self.capacity - self.count):
+            new.append([None])
+        self.s = new
+
+    def print(self):
+        result = []
+        i = 0
+        for s in range(self.count):
+            result.append(f'<{self.s[i%self.capacity]}>')
+            i += 1
+        for s in range(self.capacity - self.count):
+            result.append('<_>')
+        result.reverse()
+        string = ""
+        for s in result:
+            string += f'{s} '
+        return string
--- Simulator/main.py
+++ Simulator/main.py
@@ -1,24 +1,35 @@
 import os
 from Queue import Queue
+from Stack import Stack
 
 
-def print_status(queue):
-    print(f'''
+def print_status(item):
+    if type(item) == Queue:
+        print(f'''
 Queue Simulator
-
-Queue: {queue.print()}
+Queue: {item.print()}
 ''')
-    if queue.last_pop:
-        print(f'Popped: {queue.last_pop}')
-        queue.last_pop = None
-    print_help()
+        if item.last_pop:
+            print(f'Popped: {item.last_pop}')
+            item.last_pop = None
+        print_help()
+    elif type(item) == Stack:
+        print(f'''  
+Stack Simulator
+Stack: {item.print()}
+''')
+        if item.last_pop:
+            print(f'Popped: {item.last_pop}')
+            item.last_pop = None
+        print_help(item)
 
 
-def print_help():
-    print('''
-a, append       -   append value to the queue
-p, pop          -   pop first element from the queue
-s, sort         -   sort queue the queue
+def print_help(item):
+    struct = "queue" if type(item) == Queue else "stack"
+    print(f'''
+a, append       -   append value to the {struct}
+p, pop          -   pop last element from the {struct}
+s, sort         -   sort queue the {struct}
 q, quit, exit   -   terminate program
     ''')
 
@@ -39,41 +50,81 @@ dictionary = {
     "exit": 0
 }
 cls()
-print("The Queue Simulator")
-ok = False
-capacity = 0
-while not ok:
-    try:
-        capacity = int(input("Set capacity of the queue> "))
-        if capacity <= 0:
-            raise ValueError
-        ok = True
-    except ValueError:
-        print("Invalid value!")
+a = str(input("Choose queue or stack> "))
+if a == 'queue':
+    print("The Queue Simulator")
+    ok = False
+    capacity = 0
+    while not ok:
+        try:
+            capacity = int(input("Set capacity of the queue> "))
+            if capacity <= 0:
+                raise ValueError
+            ok = True
+        except ValueError:
+            print("Invalid value!")
 
-q = Queue(capacity)
-while True:
-    cls()
-    print_status(q)
-    choice = dictionary.get(input("> ").lower(), -1)
-    if choice == 1:
-        value = input("value> ")
-        if not q.append(value):
-            print("Queue is full")
-    elif choice == 2:
-        value = q.pop()
-        if not value:
-            print("Queue is empty")
-    elif choice == 3:
-        q.sort()
-    elif choice == 0:
-        break
-    else:
-        print("Invalid command")
-        print_help()
-    q.print()
-    if q.is_empty():
-        print("The queue is empty")
-    else:
-        print("The queue is not empty")
+    q = Queue(capacity)
+    while True:
+        cls()
+        print_status(q)
+        choice = dictionary.get(input("> ").lower(), -1)
+        if choice == 1:
+            value = input("value> ")
+            if not q.append(value):
+                print("Queue is full")
+        elif choice == 2:
+            value = q.pop()
+            if not value:
+                print("Queue is empty")
+        elif choice == 3:
+            q.sort()
+        elif choice == 0:
+            break
+        else:
+            print("Invalid command")
+            print_help()
+        q.print()
+        if q.is_empty():
+            print("The queue is empty")
+        else:
+            print("The queue is not empty")
+if a == 'stack':
+    print("The Stack Simulator")
+    ok = False
+    capacity = 0
+    while not ok:
+        try:
+            capacity = int(input("Set capacity of the stack> "))
+            if capacity <= 0:
+                raise ValueError
+            ok = True
+        except ValueError:
+            print("Invalid value!")
+
+    s = Stack(capacity)
+    while True:
+        cls()
+        print_status(s)
+        choice = dictionary.get(input("> ").lower(), -1)
+        if choice == 1:
+            value = input("value> ")
+            if not s.append(value):
+                print("Stack is full")
+        elif choice == 2:
+            value = s.pop()
+            if not value:
+                print("Stack is empty")
+        elif choice == 3:
+            s.sort()
+        elif choice == 0:
+            break
+        else:
+            print("Invalid command")
+            print_help()
+        s.print()
+        if s.is_empty():
+            print("The stack is empty")
+        else:
+            print("The stack is not empty")
 print("bye")
